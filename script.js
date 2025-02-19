38a7562895aa8089b2242ce8813d0c42de901e23
Gabriel Oliveira <gabrieloliverasantos162@gmail.com>
2025-02-19 02:51:18 -0300

    agora finalizado

---
+++ script.js
@@ -0,0 +1,25 @@
+ScrollReveal().reveal('.div-filho',{
+    duration: 1500, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'left', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
+ScrollReveal().reveal('.section2',{
+    duration: 1500, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'left', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
+ScrollReveal().reveal('.text',{
+    duration: 2500, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'right', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
+ScrollReveal().reveal('.section3',{
+    duration: 1500, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'left', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
+ScrollReveal().reveal('.section4',{
+    duration: 1500, // duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'bottom', // origem do efeito: o conteudo virá da direita para a esquerda
+    distance : '50px', // distancia que o elemento "viaja" antes de aparecer
+})
